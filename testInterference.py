3358389ed78ca374f8d4b99e7f6cf8076ef948a4
Friedrich Groestlinger <friedrich.groestlinger@plasmo.eu>
2014-10-27 18:29:00 +0100

    parameters used for plotting

--- testInterference.py
+++ testInterference.py
@@ -4,16 +4,25 @@ Created on Fri Oct 24 16:41:16 2014
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import rc
 
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern Roman'], 'sans-serif': ['Computer Modern Sans serif']})
+rc('text', usetex=True)
 
 c = 3E8 # m s-1
 l0 = 8E-7 # m
 dl = 2E-8 #m
+
 num_v = 100
-num_t = 10000
-num_dt = 100
-T = 1E-12 # s
-dt = 1E-13 # s
+num_t = 3000
+num_dt = 2000
+
+#num_v = 100
+#num_t = 100
+#num_dt = 100
+
+T = 5.0E-11 # s
+dt = 1.5E-13 # s
 k_1 = 0.5
 k_2 = 0.5
 
@@ -28,6 +37,8 @@ plt.clf()
 N_v = lambda v: 1.0 / (np.sqrt(2 * np.pi) * dv) * np.exp(- (v - v0) ** 2 / (2 * dv ** 2))
 vs = np.linspace(v0 - v_oo, v0 + v_oo, num_v)
 #plt.plot(vs, N_v(vs), 'k-')
+#plt.xlabel(r'\nu\ (s)')
+#plt.ylabel(r'$N(\nu)$')
 
 V = lambda x, t, v, V0: V0 * np.exp(- 2 * np.pi * 1j * (v /c * x - v * t))
 #x = np.linspace(0, 1E-5, 1000)
@@ -43,9 +54,9 @@ V_v = lambda dt: np.array([V(0.0, ts - dt, v, N_v(v)) for v in vs])
 #for x in V_v(0.0) + V_v(dt):
 #    plt.plot(x)
 
-dts = np.linspace(0.0, dt, num_dt)
+dts = np.linspace(- dt, dt, num_dt)
 
-V_v_dt = lambda dt: np.sum(k_1 * V_v(0.0) + k_2 * V_v(dt), 0)
+V_v_dt = lambda dt: np.sum(k_1 * V_v(0.0) + k_2 * V_v(dt), 0) * (vs[1] - vs[0])
 #for i, y in enumerate(dts):
 #    z = V_v_dt(y)
 #    plt.subplot(len(dts) + 1, 2, (2 * i) + 2)
@@ -55,8 +66,15 @@ V_v_dt = lambda dt: np.sum(k_1 * V_v(0.0) + k_2 * V_v(dt), 0)
 #        plt.plot(x)
 #
 
-I_dt = lambda dt: V_v_dt(dt).conjugate() * V_v_dt(dt)
+def I_dt(dt):
+    V = V_v_dt(dt)
+    return V.conjugate() * V
+    
+    
 I = np.array([np.sum(I_dt(t)) for t in dts])
-plt.plot(I)
-
+plt.plot(dts, np.abs(I), 'k-')
+ax = plt.axis()
+plt.axis([dts[0], dts[-1], ax[2], ax[3]])
+plt.ylabel(r'I(\Delta t)')
+plt.xlabel(r'\Delta t\ (s)')
 plt.show()
